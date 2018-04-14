91966429fb74a8e52c456b21381f8e9f9e6da781
dimakozin <dimakozin@gmail.com>
2018-04-14 22:42:26 +0300

    Натянута верстка

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,14 @@
 const path = require('path')
 const express = require('express')
 
+answer = {
+        places : [
+        { description : "test place", coordinates : "59.201145, 39.873853"},
+        { description : "another test place", coordinates : "59.201145, 39.873853", img: "https://avatars.mds.yandex.net/get-ugc/217759/2a0000015faf7b98b1ee8b94dcb5703fd1b0/X5L"}
+        ]
+}
+
+
 module.exports = (app, db) => {
 
         app.use('/static', express.static('static'));
@@ -17,14 +25,14 @@ module.exports = (app, db) => {
                 response.sendFile(path.resolve(__dirname, '..', 'views', 'maps.html'))
         })
 
-        app.get('/index.html', (request,response) => {
+        app.get('/index.html', (request,response)=>{
                response.redirect('/')
         })
 
         app.get('/api/popular', (request, response) => {
-            db.collection('places').find({}).toArray((err, result) => {
-              response.send(result)
-            })
+                db.collection('places').find({}).toArray((err, result) => {
+                  response.send(result)
+                })
         })
 
 }
